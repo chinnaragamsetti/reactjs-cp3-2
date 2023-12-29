ada7200d1f6c875379b65aae4ce7c5e7a4d9ec16
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2023-12-29 21:55:37 +0530

    Update index.js

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -1,14 +1,12 @@
-// Write your code here.
-
 import './index.css'
 
 const Card = props => {
   const {cardDetails} = props
-  const {id, title, description, imgUrl, className} = cardDetails
+  const {title, description, imgUrl, className} = cardDetails
   return (
     <li className={`${className} cardItem`}>
       <h1 className="title">{title}</h1>
-      <p className="description">{description}</p>
+      <p className="">{description}</p>
       <div className="imageContainer">
         <img src={imgUrl} className="image" alt={title} />
       </div>
@@ -17,3 +15,4 @@ const Card = props => {
 }
 
 export default Card
+
